b38a761029bc2b08badc196113b0590b1683a4b4
Raja Naveed <rajadahri11@gmail.com>
2023-05-19 10:16:19 +0500

    added email js to send email to owner

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React,  { useRef }  from "react";
 import { fadeIn } from "../variants";
 import { motion } from "framer-motion";
-
-
-
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_bqoutzq",
+        "template_1atlknc",
+        form.current,
+        "NNhk_oKV8W4kOfmsU"
+      )
+      .then(
+        (result) => {
+          alert(result.text);
+        },
+        (error) => {
+          alert(error.text);
+        }
+      );
+  };
+
   return (
     <section className="section" id="contact">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row">
           {/* text  */}
           <motion.div
-           variants={fadeIn("right", 0.3)}
-           initial="hidden"
-           whileInView={"show"}
-           viewport={{ once: false, amount: 0.3 }}
-          className="flex-1 flex justify-start items-center">
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex justify-start items-center"
+          >
             <div>
               <h4 className="text-xl text-accent uppercase font-medium mb-2 tracking-wide">
                 Get in Touch
@@ -27,12 +48,15 @@ const Contact = () => {
             </div>
           </motion.div>
           {/* form  */}
-          <motion.form 
-           variants={fadeIn("left", 0.3)}
-           initial="hidden"
-           whileInView={"show"}
-           viewport={{ once: false, amount: 0.3 }}
-          className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start">
+          <motion.form
+            ref={form}
+            onSubmit={sendEmail}
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start"
+          >
             <input
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
               type="text"
@@ -47,7 +71,7 @@ const Contact = () => {
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
               placeholder="Your Message"
             ></textarea>
-            <button className="btn btn-lg">Send Message</button>
+            <button className="btn btn-lg" type="submit">Send Message</button>
           </motion.form>
         </div>
       </div>
